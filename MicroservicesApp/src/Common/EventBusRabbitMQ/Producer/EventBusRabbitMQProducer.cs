c51db29d97b780d305e70067816869b331789ce8
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2020-07-30 19:33:58 +0400

    Created queue for Checkout Basket

---
+++ MicroservicesApp/src/Common/EventBusRabbitMQ/Producer/EventBusRabbitMQProducer.cs
@@ -0,0 +1,45 @@
+﻿using EventBusRabbitMQ.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ.Producer
+{
+    public class EventBusRabbitMQProducer
+    {
+        private readonly IRabbitMQConnection _connection;
+
+        public EventBusRabbitMQProducer(IRabbitMQConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void PublishBasketCheckout(string queueName, BasketCheckoutEvent publishModel)
+        {
+            using (var channel = _connection.CreateModel())
+            {
+                channel.QueueDeclare(queueName, true, false, true, null);
+                var message = JsonConvert.SerializeObject(publishModel);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties basicProperties = channel.CreateBasicProperties();
+                basicProperties.Persistent = true;
+                basicProperties.DeliveryMode = 2;
+
+                channel.ConfirmSelect();
+                channel.BasicPublish("",queueName,basicProperties,body);
+                channel.WaitForConfirmsOrDie();
+
+                channel.BasicAcks += (sender, eventArgs) =>
+                {
+                    Console.WriteLine("RabbitMq message sent");
+                };
+                channel.ConfirmSelect();
+            }
+        }
+
+    }
+}
