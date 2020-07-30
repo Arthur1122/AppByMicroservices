d17067bade2dc9c759b7d075b35a65378cf65c87
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2020-07-30 14:25:19 +0400

    Added Entites for Basket.API project

---
+++ src/Basket/Basket.API/Entities/BasketCartItem.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class BasketCartItem
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+
+    }
+}
