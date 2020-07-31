1bc61cbd2584ce389ead04ce6432f5440e05c76c
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2020-07-31 16:46:25 +0400

    Repository Implementation Developments with usering EF.Core Context Objects

---
+++ MicroservicesApp/src/Ordering/Ordering.InfoStructure/Repository/OrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using Ordering.Infostructure.Repository.Base;
+using Ordering.InfoStructure.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infostructure.Repository
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                      .Where(o => o.UserName == userName)
+                      .ToListAsync();
+
+            return orderList;
+        }        
+    }
+}
